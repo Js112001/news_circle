39df8cb87e312016b0b0ace3d6abdbcdae026896
Js112001 <jabbarshaikh247@gmail.com>
2025-01-04 18:27:26 +0530

    added events for saving, removing & retrieving articles

--- lib/modules/news/presentation/blocs/home_bloc.dart
+++ lib/modules/news/presentation/blocs/home_bloc.dart
@@ -3,6 +3,9 @@ import 'package:equatable/equatable.dart';
 import 'package:news_circle/modules/news/domain/entities/article_entity.dart';
 import 'package:news_circle/modules/news/domain/entities/article_request_entity.dart';
 import 'package:news_circle/modules/news/domain/usecases/get_articles.dart';
+import 'package:news_circle/modules/news/domain/usecases/get_saved_articles.dart';
+import 'package:news_circle/modules/news/domain/usecases/remove_saved_article.dart';
+import 'package:news_circle/modules/news/domain/usecases/save_article.dart';
 
 part 'home_event.dart';
 
@@ -10,10 +13,21 @@ part 'home_state.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
   final GetArticlesUseCase _getArticlesUseCase;
+  final GetSavedArticlesUseCase _getSavedArticlesUseCase;
+  final SaveArticleUseCase _saveArticleUseCase;
+  final RemoveSavedArticleUseCase _removeSavedArticleUseCase;
 
-  HomeBloc(this._getArticlesUseCase) : super(InitialState()) {
+  HomeBloc(
+    this._getArticlesUseCase,
+    this._getSavedArticlesUseCase,
+    this._saveArticleUseCase,
+    this._removeSavedArticleUseCase,
+  ) : super(InitialState()) {
     on<InitialEvent>(_onInitialEvent);
     on<GetArticleEvent>(_onGetArticlesEvent);
+    on<GetSavedArticleEvent>(_onGetSavedArticleEvent);
+    on<RemoveSavedArticleEvent>(_onRemoveSavedArticleEvent);
+    on<SaveArticleEvent>(_onSaveArticleEvent);
   }
 
   void _onInitialEvent(InitialEvent event, Emitter<HomeState> emit) async {
@@ -35,12 +49,48 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
         ),
       );
 
+      var allArticles = result.articles;
+      final savedArticles = await _getSavedArticlesUseCase();
+
+      for (var savedArticle in savedArticles) {
+        var index = allArticles.indexOf(savedArticle);
+        if (index >= 0) {
+          allArticles[index].copyWith(id: savedArticle.id);
+        }
+      }
+
       emit(GetArticleSuccessState(
-        articles: result.articles,
+        articles: allArticles,
         isPaginated: false,
       ));
     } catch (e) {
       emit(GetArticlesFailureState(e.toString()));
+    }
+  }
+
+  void _onGetSavedArticleEvent(
+      GetSavedArticleEvent event, Emitter<HomeState> emit) async {
+    // final result = await _getSavedArticlesUseCase();
+    // emit()
+  }
+
+  void _onRemoveSavedArticleEvent(
+      RemoveSavedArticleEvent event, Emitter<HomeState> emit) async {
+    final result = await _removeSavedArticleUseCase(params: event.article.id);
+    if (result >= 1) {
+      emit(SavedArticleSuccessState('Article removed from save'));
+    } else {
+      emit(SavedArticleFailureState('Failed to remove article from save'));
+    }
+  }
+
+  void _onSaveArticleEvent(
+      SaveArticleEvent event, Emitter<HomeState> emit) async {
+    final result = await _saveArticleUseCase(params: event.article);
+    if (result >= 1) {
+      emit(SavedArticleSuccessState('Article added to save'));
+    } else {
+      emit(SavedArticleFailureState('Failed to add article to save'));
     }
   }
 }
